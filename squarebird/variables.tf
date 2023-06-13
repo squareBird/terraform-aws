d8a96725b2208dd7115ab74f66efb763d6c982b8
squarebird <fails9503@gmail.com>
2023-06-13 16:19:16 +0900

    Feat: "변수 수정"
    - 사용하지 않는 변수 주석처리

--- squarebird/variables.tf
+++ squarebird/variables.tf
@@ -1,28 +1,28 @@
-variable "vpc_cidr" {
-  description = "VPC CIDR block"
-  type        = string
-}
-
-variable "subnet_cidrs" {
-  description = "List of subnet CIDR blocks"
-  type        = list(string)
-}
-
-variable "instance_count" {
-  description = "Number of EC2 instances"
-  type        = number
-}
-
-variable "instance_type" {
-  description = "Type of EC2 instance"
-  type        = string
-}
-
-variable "subnet_id" {
-  description = "Subnet ID"
-  type        = string
-}
-
+#variable "vpc_cidr" {
+#  description = "VPC CIDR block"
+#  type        = string
+#}
+#
+#variable "subnet_cidrs" {
+#  description = "List of subnet CIDR blocks"
+#  type        = list(string)
+#}
+#
+#variable "instance_count" {
+#  description = "Number of EC2 instances"
+#  type        = number
+#}
+#
+#variable "instance_type" {
+#  description = "Type of EC2 instance"
+#  type        = string
+#}
+#
+#variable "subnet_id" {
+#  description = "Subnet ID"
+#  type        = string
+#}
+#
 
 
 # Terraform Cloud와 연동을위해 변수 정의
