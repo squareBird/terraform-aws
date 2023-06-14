04e165566f369f752426c1907430a3a4e591250e
liam <liam.jg@vanilacorp.com>
2023-06-14 17:55:29 +0900

    Feat: "S3 생성 비활성화"

--- squarebird/s3.tf
+++ squarebird/s3.tf
@@ -1,11 +1,11 @@
-resource "aws_s3_bucket" "terraform_state_bucket" {
-  bucket = "squarebird-terraform-state"
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle {
-    prevent_destroy = false
-  }
-}
+#resource "aws_s3_bucket" "terraform_state_bucket" {
+#  bucket = "squarebird-terraform-state"
+#
+#  versioning {
+#    enabled = true
+#  }
+#
+#  lifecycle {
+#    prevent_destroy = false
+#  }
+#}
